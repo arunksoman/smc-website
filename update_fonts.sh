f08f11f028d66a7e284d04e6597860a3ef14d691
Balasankar "Balu" C <balasankar@gitlab.com>
2018-04-18 13:48:30 +0530

    Fix typo in variable name

--- update_fonts.sh
+++ update_fonts.sh
@@ -2,8 +2,8 @@ mkdir -p downloads/fonts
 cd downloads/fonts
 fonts=(anjalioldlipi rachana meera manjari raghumalayalamsans dyuthi keraleeyam uroob chilanka karumbi)
 for font in ${fonts[@]}; do
-        mkdir font
-        cd font
+        mkdir $font
+        cd $font
         version=$(curl -sSL https://gitlab.com/api/v4/projects/smc%2Ffonts%2F${font}/repository/tags| jq '.[0]["name"]' | tr -d '"')
         wget "https://gitlab.com/smc/fonts/${font}/-/jobs/artifacts/${version}/download?job=release" -O artifacts.zip
         unzip artifacts.zip
